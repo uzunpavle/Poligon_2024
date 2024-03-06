bae0d35d3b140dd063b2b03dfc555cfda6db5ff5
Ucenik <Ucenik@DESKTOP-A02KV2H>
2024-03-06 20:07:03 +0100

    Rad na casu 6.3.2024. Dva zadatka odardjena sto je radredni dao

--- Program.cs
+++ Program.cs
@@ -93,7 +93,7 @@ namespace Poligon_2024
             Console.WriteLine("Obim novog poligona je {0}", obim);
             */
             /**********************************************************************************************/
-
+            /*
             Poligon prvi = new Poligon(4);
             prvi.temena[0] = new Tacka(1, 1);
             prvi.temena[1] = new Tacka(3, 1);
@@ -102,6 +102,91 @@ namespace Poligon_2024
             Vektor AB = new Vektor(prvi.temena[0], prvi.temena[1]);
             Vektor CD = new Vektor(prvi.temena[2], prvi.temena[3]);
             Console.WriteLine(Funkcije.Presek(AB, CD));
+            */
+            /*******************************************************************************************/
+            /*
+            Poligon prvi = new Poligon(4);
+            prvi.temena[0] = new Tacka(6, 8);
+            prvi.temena[1] = new Tacka(4, 6);
+            prvi.temena[2] = new Tacka(5, 3);
+            prvi.temena[3] = new Tacka(8, 6);
+
+            Vektor AB = new Vektor(prvi.temena[0], prvi.temena[1]);
+            Vektor BC = new Vektor(prvi.temena[1], prvi.temena[2]);
+            Vektor CD = new Vektor(prvi.temena[2], prvi.temena[3]);
+            Vektor DA = new Vektor(prvi.temena[3], prvi.temena[0]);
+            Vektor[] vektori = new Vektor[4];
+            vektori[0] = AB;
+            vektori[1] = BC;
+            vektori[2] = CD;
+            vektori[3] = DA;
+
+            bool pravi_uglovi = false;
+            bool jeste = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if ((Vektor.Skalarni(vektori[i], vektori[(i + 1) % 4]) == 0) && (Vektor.Skalarni(vektori[(i + 1) % 4], vektori[(i + 2) % 4]) == 0))
+                {
+                    if (pravi_uglovi == false)
+                    {
+                        pravi_uglovi = true;
+                        continue;
+                    }
+                    else
+                    {
+                        jeste = false;
+                        break;
+                    }
+                }
+            }
+            if ((jeste == true) && (pravi_uglovi == true))
+            {
+                Console.WriteLine("Jeste");
+            }
+            else
+            {
+                Console.WriteLine("Nije");
+            }
+            */
+            /**********************************************************************************************************/
+
+            Poligon prvi = new Poligon(3);
+            prvi.temena[0] = new Tacka(6, 8);
+            prvi.temena[1] = new Tacka(2, 6);
+            prvi.temena[2] = new Tacka(8, 2);
+
+            Vektor AB = new Vektor(prvi.temena[0], prvi.temena[1]);
+            Vektor BC = new Vektor(prvi.temena[1], prvi.temena[2]);
+            Vektor CA = new Vektor(prvi.temena[2], prvi.temena[0]);
+
+            Vektor[] vektori = new Vektor[3];
+            vektori[0] = AB;
+            vektori[1] = BC;
+            vektori[2] = CA;
+
+            double[] uglovi = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                double a = Vektor.Skalarni(vektori[i], vektori[(i + 1) % 3]);
+                uglovi[i] = a;
+            }
+            double max = uglovi[0];
+            for (int i = 1; i < 3; i++)
+            {
+                if (uglovi[i] > max) max = uglovi[i];
+            }
+            if (max == 0)
+            {
+                Console.WriteLine("Prav");
+            }
+            else if (max > 0)
+            {
+                Console.WriteLine("Tup");
+            }
+            else
+            {
+                Console.WriteLine("Ostar");
+            }
         }
     }
 }
